ceeaee40fda51d51ad1cbfa3cb762ad607525d83
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2023-06-05 14:07:58 -0300

    refactor: change src directory name

---
+++ source/01-intro/aula01.py
@@ -0,0 +1,12 @@
+""" aula01 """
+
+print('Olá Mundo!')
+
+
+def somar(nuemro_01, numero_02):
+    """ soma dois números """
+    return nuemro_01 + numero_02
+
+
+SOMA = somar(10.3, 4.7)
+print(SOMA)
