f28a7bf52de99dec7c06ee7d35e2a3ba9ab5ef45
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2023-06-05 14:16:11 -0300

    feat: add aula02

---
+++ source/01-intro/aula02.py
@@ -0,0 +1,32 @@
+""" Aula 02 -Identificadores e Keywords """
+
+# Palavras reservadas
+# True, Flase, None, and, import, lambda
+
+# Identificadores
+# nome de variáveis, funções, classes
+# case sensitive
+# devem iniciar com letra ou _
+# sem espaços
+# sem caracteres especiais
+
+nome = 'Maria'
+idade = 20
+
+Nome = 'João'
+nome_completo = 'Maria da Silva'
+
+# Good practice
+c = 10 # X
+contador = 10 
+
+# - Contantes
+PI = 3.14
+
+idade = 18
+MAIORIDADE = 18
+
+if idade >= MAIORIDADE:
+    print('maior de idade')
+else:
+    print('menor de idade')
