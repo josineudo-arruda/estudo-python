7038d2bde4ccb5e0ad4b6c01baf5f34771e5a958
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2023-06-05 14:23:47 -0300

    feat: add aula03

---
+++ source/01-intro/aula03.py
@@ -0,0 +1,16 @@
+""" Aula 03 - Comentários """
+
+# Comentário de única linha
+
+'''
+Comentário de bloco
+'''
+
+# Comentar VSCode Ctrl + ;
+
+# Atribui o valor 10 a variável numero_01
+numero_01 = 10
+numero_02 = 60
+# numero_03 = 20
+
+print(numero_01, numero_02)
