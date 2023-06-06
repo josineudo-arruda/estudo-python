1b168843da8921574b0cc19b5b76296f7ba57479
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2023-06-06 14:09:17 -0300

    feat: add aula06 data convertion

---
+++ source/01-intro/aula06.py
@@ -0,0 +1,19 @@
+""" Aula 06 - Conversão de Tipo """
+
+# Convsersão de tipo implícita 
+leitura = 5.54
+peso = 3
+total = leitura * peso
+print(total, type(total))
+
+# Conversão explícita
+soma = 13.5 + float('3.5')
+# soma = 13.5 + float('abc')
+print(soma, type(soma))
+
+idade = int('32')
+print(idade, type(idade))
+
+idade = str(idade)
+print(idade, type(idade))
+
